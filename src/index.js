06dd0a99f03e7a626dd8ed995c9bdf54b59c4b19
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2020-03-03 19:10:48 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  return true
+    let testArray = str.split('');
+    let configLength = bracketsConfig.length;
+    
+    if(testArray.length % 2 !== 0){
+        return false
+    }
+
+    for (let i = 0; i < configLength;) {
+        let checkedBrackets = bracketsConfig[i].join('');
+        if (str.includes(checkedBrackets)) {
+            str = str.replace(checkedBrackets, '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+
+    let result = str.length ? false : true;
+
+    return result;
 }
